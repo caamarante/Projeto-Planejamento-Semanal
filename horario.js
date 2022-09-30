f65310740f01084860f7982518f55ed952be84f7
Ana Carolina <ana.amarante.pb@compasso.com.br>
2022-09-30 11:16:10 -0300

    Ajustando para quando a hora ou o minuto for menor que 10 adicionar um 0 antes

--- horario.js
+++ horario.js
@@ -1,6 +1,22 @@
 monName = new Array ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro" , "Novembro", "Dezembro");
 now = new Date;
 
-document.getElementById("calendario").innerHTML = (now.getDate () + " de " + monName [now.getMonth() ] +  " de " + now.getFullYear ());
+function verificaHoras() {
+    var hora = now.getHours();
+    if(hora < 10){
+        hora="0"+hora;
+    }
+    return hora;
+}
+function verificaMinutos() {
+    var min = now.getMinutes();
+    if(min < 10){
+        min="0"+min;
+    }
+    return min;
+}
 
-document.getElementById("relogio").innerHTML =(now.getHours() + ":" + now.getMinutes());
+document.getElementById("calendario").innerHTML = (now.getDate () + " de " + monName [now.getMonth() ] +  " de " + now.getFullYear ());
+setInterval (verificaHoras, 500);
+setInterval (verificaMinutos, 500);
+document.getElementById("relogio").innerHTML =(verificaHoras() + ":" + now.getMinutes());
