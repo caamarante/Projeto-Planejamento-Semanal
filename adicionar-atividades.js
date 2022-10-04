b190853539b840efd3112cbc5030c3a599ced00d
Ana Carolina <ana.amarante.pb@compasso.com.br>
2022-10-04 10:07:55 -0300

    Adicionando uma parte de cor, o codigo não está correto mas apenas para salvar dessa maneira e ter uma ideia de como mudar na logica depois para se adequar ao objetivo

--- adicionar-atividades.js
+++ adicionar-atividades.js
@@ -86,19 +86,99 @@ botao_atividade.onclick = function adicionarAtividade() {
     console.log(guardarAtividadeHora);
 
         guardarHora.sort();
-        console.log(guardarHora)
+        console.log(guardarHora);
 
-        for (i=0; i < guardarHora.length; i++) {
-            document.getElementById("cards__horarios-lista").insertAdjacentHTML("beforeend", '<li class="cards__horarios-item">' + guardarHora[i] + '</li>');
-            document.getElementById("cards__atividades").insertAdjacentHTML("beforeend", '<ul class="cards__atividades__lista" id="' + guardarHora[i] + '"></ul>');
-            for (j=0; j < guardarAtividadeHora[0].length; j++) {
-                if (guardarHora[i] == guardarAtividadeHora[1][j]){
-                    //console.log(guardarAtividadeHora[0][j]);
-                    document.getElementById(guardarHora[i]).insertAdjacentHTML("beforeend",'<li class="cards__atividades-item"><div class="cards__atividades-item-tag"></div><p class="cards__atividades-item-texto">' + guardarAtividadeHora[0][j] + '</p><buttton class="cards__atividades-item-apagar">Apagar</buttton></li>');
+        if(dia_segunda.className == "atividades__dia_semana-item semana-laranja atividades__dia_semana-item_selecionado"){
+            for (i=0; i < guardarHora.length; i++) {
+                document.getElementById("cards__horarios-lista").insertAdjacentHTML("beforeend", '<li class="cards__horarios-item cards__horarios-item-segunda">' + guardarHora[i] + '</li>');
+                document.getElementById("cards__atividades").insertAdjacentHTML("beforeend", '<ul class="cards__atividades__lista" id="' + guardarHora[i] + '"></ul>');
+                for (j=0; j < guardarAtividadeHora[0].length; j++) {
+                    if (guardarHora[i] == guardarAtividadeHora[1][j] && guardarAtividadeHora[2][j] == 'segunda'){
+                        //console.log(guardarAtividadeHora[0][j]);
+                        document.getElementById(guardarHora[i]).insertAdjacentHTML("beforeend",'<li class="cards__atividades-item"><div class="cards__atividades-item-tag cards__atividades-item-tag-segunda"></div><p class="cards__atividades-item-texto">' + guardarAtividadeHora[0][j] + '</p><buttton class="cards__atividades-item-apagar">Apagar</buttton></li>');
+                    }
                 }
             }
         }
 
+        if(dia_terca.className == "atividades__dia_semana-item semana-verde-claro atividades__dia_semana-item_selecionado"){
+            for (i=0; i < guardarHora.length; i++) {
+                document.getElementById("cards__horarios-lista").insertAdjacentHTML("beforeend", '<li class="cards__horarios-item cards__horarios-item-terca">' + guardarHora[i] + '</li>');
+                document.getElementById("cards__atividades").insertAdjacentHTML("beforeend", '<ul class="cards__atividades__lista" id="' + guardarHora[i] + '"></ul>');
+                for (j=0; j < guardarAtividadeHora[0].length; j++) {
+                    if (guardarHora[i] == guardarAtividadeHora[1][j] && guardarAtividadeHora[2][j] == 'segunda'){
+                        //console.log(guardarAtividadeHora[0][j]);
+                        document.getElementById(guardarHora[i]).insertAdjacentHTML("beforeend",'<li class="cards__atividades-item"><div class="cards__atividades-item-tag cards__atividades-item-tag-terca"></div><p class="cards__atividades-item-texto">' + guardarAtividadeHora[0][j] + '</p><buttton class="cards__atividades-item-apagar">Apagar</buttton></li>');
+                    }
+                }
+            }
+        }
+
+        if(dia_quarta.className == "atividades__dia_semana-item semana-azul atividades__dia_semana-item_selecionado"){
+            for (i=0; i < guardarHora.length; i++) {
+                document.getElementById("cards__horarios-lista").insertAdjacentHTML("beforeend", '<li class="cards__horarios-item cards__horarios-item-quarta">' + guardarHora[i] + '</li>');
+                document.getElementById("cards__atividades").insertAdjacentHTML("beforeend", '<ul class="cards__atividades__lista" id="' + guardarHora[i] + '"></ul>');
+                for (j=0; j < guardarAtividadeHora[0].length; j++) {
+                    if (guardarHora[i] == guardarAtividadeHora[1][j] && guardarAtividadeHora[2][j] == 'segunda'){
+                        //console.log(guardarAtividadeHora[0][j]);
+                        document.getElementById(guardarHora[i]).insertAdjacentHTML("beforeend",'<li class="cards__atividades-item"><div class="cards__atividades-item-tag cards__atividades-item-tag-quarta"></div><p class="cards__atividades-item-texto">' + guardarAtividadeHora[0][j] + '</p><buttton class="cards__atividades-item-apagar">Apagar</buttton></li>');
+                    }
+                }
+            }
+        }
+        
+        if(dia_quinta.className == "atividades__dia_semana-item semana-roxo atividades__dia_semana-item_selecionado"){
+            for (i=0; i < guardarHora.length; i++) {
+                document.getElementById("cards__horarios-lista").insertAdjacentHTML("beforeend", '<li class="cards__horarios-item cards__horarios-item-quinta">' + guardarHora[i] + '</li>');
+                document.getElementById("cards__atividades").insertAdjacentHTML("beforeend", '<ul class="cards__atividades__lista" id="' + guardarHora[i] + '"></ul>');
+                for (j=0; j < guardarAtividadeHora[0].length; j++) {
+                    if (guardarHora[i] == guardarAtividadeHora[1][j] && guardarAtividadeHora[2][j] == 'segunda'){
+                        //console.log(guardarAtividadeHora[0][j]);
+                        document.getElementById(guardarHora[i]).insertAdjacentHTML("beforeend",'<li class="cards__atividades-item"><div class="cards__atividades-item-tag cards__atividades-item-tag-quinta"></div><p class="cards__atividades-item-texto">' + guardarAtividadeHora[0][j] + '</p><buttton class="cards__atividades-item-apagar">Apagar</buttton></li>');
+                    }
+                }
+            }
+        }
+        
+        if(dia_sexta.className == "atividades__dia_semana-item semana-azul-claro atividades__dia_semana-item_selecionado"){
+            for (i=0; i < guardarHora.length; i++) {
+                document.getElementById("cards__horarios-lista").insertAdjacentHTML("beforeend", '<li class="cards__horarios-item cards__horarios-item-sexta">' + guardarHora[i] + '</li>');
+                document.getElementById("cards__atividades").insertAdjacentHTML("beforeend", '<ul class="cards__atividades__lista" id="' + guardarHora[i] + '"></ul>');
+                for (j=0; j < guardarAtividadeHora[0].length; j++) {
+                    if (guardarHora[i] == guardarAtividadeHora[1][j] && guardarAtividadeHora[2][j] == 'segunda'){
+                        //console.log(guardarAtividadeHora[0][j]);
+                        document.getElementById(guardarHora[i]).insertAdjacentHTML("beforeend",'<li class="cards__atividades-item"><div class="cards__atividades-item-tag cards__atividades-item-tag-sexta"></div><p class="cards__atividades-item-texto">' + guardarAtividadeHora[0][j] + '</p><buttton class="cards__atividades-item-apagar">Apagar</buttton></li>');
+                    }
+                }
+            }
+        }
+        
+        if(dia_sabado.className == "atividades__dia_semana-item semana-rosa atividades__dia_semana-item_selecionado"){
+            for (i=0; i < guardarHora.length; i++) {
+                document.getElementById("cards__horarios-lista").insertAdjacentHTML("beforeend", '<li class="cards__horarios-item cards__horarios-item-sabado">' + guardarHora[i] + '</li>');
+                document.getElementById("cards__atividades").insertAdjacentHTML("beforeend", '<ul class="cards__atividades__lista" id="' + guardarHora[i] + '"></ul>');
+                for (j=0; j < guardarAtividadeHora[0].length; j++) {
+                    if (guardarHora[i] == guardarAtividadeHora[1][j] && guardarAtividadeHora[2][j] == 'segunda'){
+                        //console.log(guardarAtividadeHora[0][j]);
+                        document.getElementById(guardarHora[i]).insertAdjacentHTML("beforeend",'<li class="cards__atividades-item"><div class="cards__atividades-item-tag cards__atividades-item-tag-sabado"></div><p class="cards__atividades-item-texto">' + guardarAtividadeHora[0][j] + '</p><buttton class="cards__atividades-item-apagar">Apagar</buttton></li>');
+                    }
+                }
+            }
+        }
+        
+        if(dia_domingo.className == "atividades__dia_semana-item semana-vermelho-claro atividades__dia_semana-item_selecionado"){
+            for (i=0; i < guardarHora.length; i++) {
+                document.getElementById("cards__horarios-lista").insertAdjacentHTML("beforeend", '<li class="cards__horarios-item cards__horarios-item-domingo">' + guardarHora[i] + '</li>');
+                document.getElementById("cards__atividades").insertAdjacentHTML("beforeend", '<ul class="cards__atividades__lista" id="' + guardarHora[i] + '"></ul>');
+                for (j=0; j < guardarAtividadeHora[0].length; j++) {
+                    if (guardarHora[i] == guardarAtividadeHora[1][j] && guardarAtividadeHora[2][j] == 'segunda'){
+                        //console.log(guardarAtividadeHora[0][j]);
+                        document.getElementById(guardarHora[i]).insertAdjacentHTML("beforeend",'<li class="cards__atividades-item"><div class="cards__atividades-item-tag cards__atividades-item-tag-domingo"></div><p class="cards__atividades-item-texto">' + guardarAtividadeHora[0][j] + '</p><buttton class="cards__atividades-item-apagar">Apagar</buttton></li>');
+                    }
+                }
+            }
+        }
+        
         //------------------------
         console.log(selectDia.value)
 
